bcbe6aff742b350923fb25d0261a6d6eb8c0bc1f
rfacun <rossi_facundo@outlook.com>
2023-05-26 14:05:55 -0300

    updated scripts

--- blocks.h
+++ blocks.h
@@ -2,11 +2,11 @@
 static const Block blocks[] = {
     /*Icon*/    /*Command*/    /*Update Interval*/    /*Update Signal*/
     //{"", "pkgs",      1,      0},
-    {"", "$HOME/.local/bin/statusbar/vol",         0,      1},
-    {"", "$HOME/.local/bin/statusbar/disk",        3600,   0},
-    {"", "$HOME/.local/bin/statusbar/memory",      5,      0},
-    {"", "$HOME/.local/bin/statusbar/inet",        5,      0},
-    {"", "$HOME/.local/bin/statusbar/datetime",    1,      0},
+    {"", "$STATUSBAR/vol",         0,      1},
+    {"", "$STATUSBAR/disk",        3600,   0},
+    {"", "$STATUSBAR/memory",      5,      0},
+    {"", "$STATUSBAR/inet",        5,      0},
+    {"", "$STATUSBAR/datetime",    1,      0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
