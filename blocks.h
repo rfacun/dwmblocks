c21f762d423b45cc4ac102005ec4829e86be3a8b
rfacun <rossi_facundo@outlook.com>
2023-05-29 21:44:56 -0300

    added kb layout display

--- blocks.h
+++ blocks.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
     /*Icon*/    /*Command*/    /*Update Interval*/    /*Update Signal*/
-    //{"", "pkgs",      1,      0},
+    {"", "$STATUSBAR/kb",          0,      2},
     {"", "$STATUSBAR/vol",         0,      1},
     {"", "$STATUSBAR/disk",        3600,   0},
     {"", "$STATUSBAR/memory",      5,      0},
@@ -10,5 +10,5 @@ static const Block blocks[] = {
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = "   ";
 static unsigned int delimLen = 5;
