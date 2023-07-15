81fe29cea7c9d600b04028293fc4d865da388288
rfacun <rossi_facundo@outlook.com>
2023-07-15 18:11:55 -0300

    added scripts

--- blocks.h
+++ blocks.h
@@ -1,13 +1,17 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-    /*Icon*/    /*Command*/     /*Update Interval*/ /*Update Signal*/
-
-    {"",    "disk",     5,  0},
-    {"",    "mem",     5,  0},
-    {"",    "inet",     5,  0},
-    {"",    "tyme",     5,  0},
+    /* icon command         update-interval update-signal */
+    //{"",    "kb_caps",      0,              3},
+    //{"",    "kb_numlock",   0,              2},
+    {"",    "kb_modif",     0,              3},
+    {"",    "kb_map",       0,              2},
+    {"",    "vol",          0,              1},
+    {"",    "disk",         5,              0},
+    {"",    "mem",          5,              0},
+    {"",    "inet",         5,              0},
+    {"",    "tyme",         5,              0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = "   ";
+static char delim[] = "";
 static unsigned int delimLen = 5;
